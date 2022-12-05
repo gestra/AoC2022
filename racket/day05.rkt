77f13cf0365ec4c275c02cdcb1a13baf7b3520cb
gestra <6391580+gestra@users.noreply.github.com>
2022-12-05 14:16:00 +0200

    Day 5: Use vectors instead of lists

--- racket/day05.rkt
+++ racket/day05.rkt
@@ -19,28 +19,39 @@
   (map string->list (take lst (- (index-of lst "") 1))))
 
 (define (parse-starting-pos positions)
-  (build-list
-   (ceiling (/ (apply max (map length  positions)) 4))
-   (lambda (x)
-     (let ([i (+ 1 (* 4 x))])
-       (for/list ([row positions]
-                  #:when (and (< i (length row)) (char-alphabetic? (list-ref row i))))
-         (list-ref row i))))))
+  (list->vector
+   (build-list
+    (ceiling (/ (apply max (map length  positions)) 4))
+    (lambda (x)
+      (let ([i (+ 1 (* 4 x))])
+        (list->vector
+         (for/list ([row positions]
+                    #:when (and (< i (length row)) (char-alphabetic? (list-ref row i))))
+           (list-ref row i))))))))
 
 (define (apply-procedure stacks procedure new-model?)
   (let* ([re (regexp-match #rx"move ([0-9]+) from ([0-9]+) to ([0-9]+)" procedure)]
          [quantity (string->number(list-ref re 1))]
          [from-i (- (string->number(list-ref re 2)) 1)]
          [to-i (- (string->number(list-ref re 3)) 1)])
-    (for/list ([i (in-range (length stacks))])
-      (let ([stack (list-ref stacks i)])
-        (cond
-          [(= i from-i) (list-tail stack quantity)]
-          [(= i to-i) (if new-model?
-                          (append (take (list-ref stacks from-i) quantity) stack)
-                          (append (reverse (take (list-ref stacks from-i) quantity)) stack))]
-          [else stack])))))
-                      
+    (define (vec-rev v)
+      (build-vector (vector-length v) (lambda (x) (vector-ref v (- (- (vector-length v) x) 1)))))
+    (vector-set!
+     stacks
+     to-i
+     (if new-model?
+         (vector-append
+          (vector-take (vector-ref stacks from-i) quantity)
+          (vector-ref stacks to-i))
+         (vector-append
+          (vec-rev (vector-take (vector-ref stacks from-i) quantity))
+          (vector-ref stacks to-i))))
+    (vector-set!
+     stacks
+     from-i
+     (vector-drop (vector-ref stacks from-i) quantity))
+    stacks))
+
 (define (apply-all-procedures stacks procedures new-model?)
   (cond
     [(empty? procedures) stacks]
@@ -52,11 +63,12 @@
 
 (define (solve in new-model?)
   (list->string
-   (map (lambda (x) (first x))
-        (apply-all-procedures
-         (parse-starting-pos (starting-pos in))
-         (procedures in)
-         new-model?))))
+   (vector->list
+    (vector-map (lambda (x) (vector-ref x 0))
+                (apply-all-procedures
+                 (parse-starting-pos (starting-pos in))
+                 (procedures in)
+                 new-model?)))))
 
 (printf "Part 1: ~a\n" (solve input #f))
 (printf "Part 2: ~a\n" (solve input #t))
